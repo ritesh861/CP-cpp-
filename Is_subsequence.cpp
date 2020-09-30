d604cb708d05227ab6ca307b7d00685531c4b210
ritesh861 <64116778+ritesh861@users.noreply.github.com>
2020-10-01 02:59:25 +0530

    Create Is_subsequence.cpp

---
+++ Is_subsequence.cpp
@@ -0,0 +1,22 @@
+//is_subsequence question
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0,j=0;
+        
+        while(i<s.length()&&j<t.length())
+        {
+            if(t[j]==s[i]){
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        
+        if(i!=s.length())
+        return false;
+        return true;
+    }
+};
