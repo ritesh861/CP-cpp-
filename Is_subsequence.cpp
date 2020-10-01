f694d9f88505b94cc8cc2abdcf369962399df1ef
codeshort <61627047+codeshort@users.noreply.github.com>
2020-10-01 09:45:19 +0530

    Update Is_subsequence.cpp

--- Is_subsequence.cpp
+++ Is_subsequence.cpp
@@ -1,9 +1,10 @@
 //is_subsequence question
+
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int i=0,j=0;
-        
+//using 2 pointer approach        
         while(i<s.length()&&j<t.length())
         {
             if(t[j]==s[i]){
